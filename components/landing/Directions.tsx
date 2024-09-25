9b8496585b02caae2698b8fa65bdf1d1ea4936c0
vvmelnik <vvmelnik@quasarpro.ru>
2024-09-25 11:22:31 +0300

    fix: Directions

--- components/landing/Directions.tsx
+++ components/landing/Directions.tsx
@@ -2,13 +2,13 @@ import DirectionCarousel from "@/components/DirectionCarousel";
 
 const Directions = () => {
   return (
-    <section className="mb-4">
+    <section className="mb-8 md:mb-20">
       <div className="container px-2 md:px-8">
         <div className="mb-6 flex-col items-end">
           <h2 className="mb-4 text-4xl font-bold uppercase md:text-4xl">
             Куда пойти
           </h2>
-          <p className="max-w-[250px] text-xs">
+          <p className="max-w-[250px] text-sm md:text-lg md:max-w-[550px]">
             Больше 100 действующих групп, 20 направлений для развития,
             творческие команды и технические классы. Как тут не запутаться и
             найти то самое?
